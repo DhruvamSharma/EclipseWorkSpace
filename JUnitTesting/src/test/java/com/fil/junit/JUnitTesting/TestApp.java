b18d965f5a60f69b66c8f102d7bfc085fbddfa53
dhruvamsharma <dhruvamssharma@gmail.com>
2018-07-23 11:03:18 +0530

    starting JUnit Testing

---
+++ JUnitTesting/src/test/java/com/fil/junit/JUnitTesting/TestApp.java
@@ -0,0 +1,15 @@
+package com.fil.junit.JUnitTesting;
+
+import junit.framework.TestCase;
+
+public class TestApp extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}

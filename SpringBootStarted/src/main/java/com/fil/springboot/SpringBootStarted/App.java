7d342f3e4489097a91fc8961b7ac0803fb92b7d1
dhruvamsharma <dhruvamssharma@gmail.com>
2018-07-24 09:04:17 +0530

    started spring boot

---
+++ SpringBootStarted/src/main/java/com/fil/springboot/SpringBootStarted/App.java
@@ -0,0 +1,13 @@
+package com.fil.springboot.SpringBootStarted;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
